453fb98a5dc55f9c091133893226b6f250345133
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-09-08 01:34:48 +0530

    P5

---
+++ P5.sql
@@ -0,0 +1,40 @@
+﻿-- ---------------Practical 5------------------
+
+SELECT * FROM Registration
+
+SELECT * FROM Login
+
+SELECT * FROM Agency
+
+SELECT * FROM Payment
+
+-- --------------------------------------------
+
+SELECT User_ID,FirstName,LastName,contact,city,email,password FROM Registration 
+WHERE city='Shirdi';
+
+SELECT User_ID,FirstName,LastName,contact,city,email,password FROM Registration 
+WHERE city = 'Nashik';
+
+-- -------------------------------------------
+
+SELECT count(*) FROM Payment;
+
+SELECT MIN(amount)AS SmallestPrice
+FROM Payment;
+
+SELECT MAX(amount)AS LargestPrice
+FROM Payment;
+
+SELECT AVG(amount) FROM Payment;
+
+SELECT SUM(amount) FROM Payment;
+-- -------------------------------------------
+
+SELECT * from Agency
+where hospital_name= 'abc Clinic' and aliment = 'Headache';
+
+SELECT * FROM Agency WHERE aliment = 'Stomachache' OR hospital_name='xyz Clinic';
+
+-- -------------------------------------------
+
